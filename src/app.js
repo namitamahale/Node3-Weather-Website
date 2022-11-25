e62e142c172c478af775b67e6ade0b389a524252
Namita Mahale <90029078+namitamahale@users.noreply.github.com>
2022-11-25 17:21:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'Please contact us on ',
         title: 'Help',
         name: 'Namita Mahale'
     })
